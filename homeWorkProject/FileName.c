3f68d9d8296acb89ee3f899afb9e49c41315982f
emilya88 <yuser@YUSER-PC12>
2024-09-29 19:03:35 +0400

    test

--- homeWorkProject/FileName.c
+++ homeWorkProject/FileName.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main() {
-	int a = 4;int b = 17;int c;
-	c = a + b;
-	printf("%d",a+b);
+	int myarray[] = { 18,23,98,17,34 };
+
+	int i;
+	int length = sizeof(myarray) / sizeof(myarray[0]);
+	int lowestage = myarray[0];
+	for (i =0 ; i < length; i++) {
+		if (lowestage > myarray[i]) {
+			lowestage = myarray[i];
+		}
+	}
+
+	printf("the lowst age %d", lowestage);
+
+	return 0;
 }
+
